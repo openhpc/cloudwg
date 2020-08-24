e136fe64b94b702b24cdd8b11159754165eeebee
Christopher S. Simmons <simmons@utdallas.edu>
2020-08-24 13:53:36 -0500

    setting up csim branch for suse and arm dev and testing

---
+++ packer-templates/x86_64/centos8/compute/compute_packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dnf -y update
+dnf -y config-manager --set-enabled PowerTools
+dnf -y install epel-release
+dnf -y install http://repos.openhpc.community/OpenHPC/2/CentOS_8/x86_64/ohpc-release-2-1.el8.x86_64.rpm
+dnf -y install ohpc-base
+dnf -y install ohpc-release
+dnf -y install ohpc-slurm-client
+dnf -y install wget curl python3-pip jq git make nfs-utils libnfs
+
