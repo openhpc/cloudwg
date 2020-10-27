d7f6bb4619659c4e84c658e205f496d04f1801b5
chrdowni <chrdowni@amazon.com>
2020-10-27 17:47:56 +0000

    Adjusted IAM policy to be more restrictive, moved unique parameters out of template

--- test-cluster/cfn-templates/deploy-cluster.sh
+++ test-cluster/cfn-templates/deploy-cluster.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-aws cloudformation deploy --template-file centos8-slurm-x86_64.yml --capabilities CAPABILITY_IAM --stack-name test$RANDOM --region us-west-1
+aws cloudformation deploy \
+    --template-file centos8-slurm-x86_64.yml \
+    --capabilities CAPABILITY_IAM \
+    --stack-name test$RANDOM \
+    --region us-west-1 \
+    --parameter-overrides \
+        KeyName="YOUR-KEY" \
+        HeadAMI="YOUR-CONTROLLER-AMI" \
+        ComputeAMI="YOUR-COMPUTE-AMI" \
+        LoginAMI="YOUR-COMPUTE-AMI-AGAIN"
+
+### Other useful commands
+
+# List deployed stacks:
+# aws cloudformation list-stacks --region <REGION>
+
+# Delete stack:
+# aws cloudformation delete-stack --stack-name <NAME> --region <REGION>
 
