229911ba4ce65452681717292fdc2a881eef9475
Christopher S. Simmons <simmons@utdallas.edu>
2020-11-07 11:49:10 -0600

    refactoring tutorial dir to put all 'extra' stuff into misc

--- tutorial/misc/keys/add-ssh-keys.sh
+++ tutorial/misc/keys/add-ssh-keys.sh
@@ -4,8 +4,8 @@ BASEDIR=/home/csim/git/cloudwg2/tutorial/
 
 cd $BASEDIR/bin
 
-for i in `ls ../keys/`
-do cat ../keys/$i >> ~/.ssh/authorized_keys
+for i in id_rsa*
+do cat $i #>> ~/.ssh/authorized_keys
 done
 
 chmod 600 ~/.ssh/authorized_keys
